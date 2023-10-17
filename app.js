52b87b381e4dcbfdf9a4b7d117584662b3f24372
Arthath Agary <arthathagary@gmail.com>
2023-10-17 14:59:43 +0530

    Learn the basic type cating

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Hello World!");
+function addNum(num1, num2) {
+    return num1 + num2;
+}
+var num1 = 5;
+var num2 = 10;
+console.log(addNum(num1, num2));
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
 console.log(`Hello World!`);
+
+function addNum(num1:number,num2:number){
+    return num1+num2;
+}
+
+const num1=5;
+const num2=10;
+
+console.log(addNum(num1,num2));
